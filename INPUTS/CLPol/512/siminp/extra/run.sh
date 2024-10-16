c7a4ad3736767dfa46878adaa2bb8c2854b3dfee
tuanan <lourenco.tuanan@gmail.com>
2024-10-16 00:44:33 -0300

    Inputs for all MD simulations. The LAMMPS inputs are in the simip folder. The extra folders within siminp are the files used by fftool to create LAMMPS inputs

---
+++ INPUTS/CLPol/512/siminp/extra/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mv ../simbox.xyz .
+mv ../c2c1im* .
+mv ../ntf2* .
+mv ../pack.inp .
+mv ../data.lmp .
+mv ../emim* .
+mv ../in.lmp .
+mv ../input.lmp  .
+mv ../pair.lmp  .
+mv ../alpha.ff .
+mv ../fragment.* .
+mv ../il.ff .
+mv ../fftool_in.lmp  .
+
